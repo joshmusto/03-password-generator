9b8991091c50f48f5d83f9cd20baadabf15c2204
Josh M <musto.josh@gmail.com>
2022-11-21 12:07:13 -0500

    pass 02

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Password Generator Starter Code
 
 ## Description
+This site contains a simple password generator with options for password length and included characters. Passwords can be anywhere from 8 to 128 characters in length and include any combination of lowercase letters, uppercase letters, numbers, or special characters. Preemptive alerts are coded in in the event that a password is outside of the acceptable range, or if no group of characters is selected to be included in the password. 
 
 ## Screenshot
+![screenshot of site](./assets/screenshot.PNG)
 
 ## Link to site
+https://joshmusto.github.io/03-password-generator/
--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 // Assignment code here
+
 function generatePassword() {
-  return("peeth");
   //input length of 8-128
   var passwordLength = Math.floor(Number(prompt("How long would you like your password to be? Please pick any number from 8 to 128, inclusive.")));
   if (passwordLength>7) {
     if (passwordLength<129) {
       //use lowercase? uppercase? numeric? special?
-     //generate password
-     //return generated password
-
+      useLowercase = confirm("Would you like the password to contain lowercase letters?");
+      useUppercase = confirm("Would you like the password to contain uppercase letters?");
+      useNumbers = confirm("Would you like the password to contain numbers?");
+      useSpecial = confirm("Would you like the password to contain special characters?");
+      //concatenize array of usable characters
+      if (useLowercase) lowercase="abcdefghijklmnopqrstuvwxyz"
+      else lowercase="";
+      arrayLowercase=Array.from(lowercase);
+      if (useUppercase) uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      else uppercase="";
+      arrayUppercase=Array.from(uppercase);
+      if (useNumbers) numbers="0123456789"
+      else numbers="";
+      arrayNumbers=Array.from(numbers);
+      if (useSpecial) special="!#$%&'()*+,-./:;<=>?@][^_`{|}~\"\\ "
+      else special="";
+      arraySpecial=Array.from(special);
+      //if use=true, add char string to array
+      canUse = lowercase.concat(uppercase, numbers, special);
+      //make sure that at least one character type is selected...
+      if (canUse.length>0) {
+        //console log of usable characters for bug checking
+        console.log("Usable characters: " + canUse.toString());
+        //generate password
+        //RNG
+        function getRandomInt(max) {
+          return Math.floor(Math.random() * (max+1));
+        }
+        var arrayPassword = [];
+        for (var i=0; i<passwordLength; i++) {
+          arrayPassword[i]=canUse[getRandomInt(canUse.length)];
+        }
+        //turn password array into string, remove commas, then return generated password
+        arrayPassword = arrayPassword.toString().replaceAll(",", "");
+        return arrayPassword;
+      }
+      //...otherwise throw an error
+      alert("Passwords must include at least one group of characters.");
+      return;
     }
-    else break;
-  }
+    //if password is too long
+    alert("That password would be too long. Please pick a number from 8 to 128, inclusive.");
+    }
   else {
-    alert("Please pick a number from 8 to 128, inclusive.");
-    generatePassword();
+    //password is too short
+    alert("That password wouldn't be long enough. Please pick a number from 8 to 128, inclusive.");
   }
 }
 
